2bedca6eac7b56795183789e7b925bf5cbfb6fdf
pejvan <pejvan@gmail.com>
2015-09-20 22:39:27 +0200

    Implemented the boyer-moore algorithm  with character and comparison counting. NB: the question was: additionally count and return (a) the number of character comparisons performed and (b) the number of alignments tried. However, it seems like the example notebook return the a and b in inverted order

--- ProgrammingHomework2/homework2.py
+++ ProgrammingHomework2/homework2.py
@@ -26,11 +26,61 @@ def example_1_2():
     #print(naive_with_counts(p, t))
     assert naive_with_counts(p, t) == ([0, 19], 35, 20)
 
+def boyer_moore_with_counts(p, p_bm, t):
+    """ Do Boyer-Moore matching. p=pattern, t=text, p_bm=BoyerMoore object for p """
+    i = 0
+    occurrences = []
+    num_char_comp = 0
+    num_aligments_tried = 0
+    
+    while i < len(t) - len(p) + 1:
+        shift = 1
+        mismatched = False
+        num_aligments_tried += 1
+
+        for j in range(len(p)-1, -1, -1):
+            num_char_comp += 1
+            if p[j] != t[i+j]:    
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
+                mismatched = True
+                break
+
+        if not mismatched:
+            occurrences.append(i)
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
+        i += shift
+        
+    return occurrences, num_char_comp, num_aligments_tried
+
+
+def example_2_1():
+    from bm_preproc import BoyerMoore
+    p = 'word'
+    t = 'there would have been a time for such a word'
+    lowercase_alphabet = 'abcdefghijklmnopqrstuvwxyz '
+    p_bm = BoyerMoore(p, lowercase_alphabet)
+    #print(boyer_moore_with_counts(p, p_bm, t))
+    assert boyer_moore_with_counts(p, p_bm, t) == ([40], 15, 12)
+
+def example_2_2():
+    from bm_preproc import BoyerMoore
+    p = 'needle'
+    t = 'needle need noodle needle'
+    lowercase_alphabet = 'abcdefghijklmnopqrstuvwxyz '
+    p_bm = BoyerMoore(p, lowercase_alphabet)
+    #print(boyer_moore_with_counts(p, p_bm, t))
+    assert boyer_moore_with_counts(p, p_bm, t) == ([0, 19], 18, 5)
+
 
 def main():
     example_1_1()
     example_1_2()
     print "All tests passed successfully for examples in set1"
+    example_2_1()
+    example_2_2()
    
 if __name__ == "__main__":
     main()
