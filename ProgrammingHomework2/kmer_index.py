e5b61d11b8040db917ba8be69f9925def1ec956b
pejvan <pejvan@gmail.com>
2015-09-20 21:58:35 +0200

    set up for homework 2 complete

---
+++ ProgrammingHomework2/kmer_index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""kmer_index.py: A k-mer index for indexing a text."""
+
+__author__ = "Ben Langmead"
+
+import bisect
+
+
+class Index(object):
+    """ Holds a substring index for a text T """
+
+    def __init__(self, t, k):
+        """ Create index from all substrings of t of length k """
+        self.k = k  # k-mer length (k)
+        self.index = []
+        for i in range(len(t) - k + 1):  # for each k-mer
+            self.index.append((t[i:i+k], i))  # add (k-mer, offset) pair
+        self.index.sort()  # alphabetize by k-mer
+
+    def query(self, p):
+        """ Return index hits for first k-mer of p """
+        kmer = p[:self.k]  # query with first k-mer
+        i = bisect.bisect_left(self.index, (kmer, -1))  # binary search
+        hits = []
+        while i < len(self.index):  # collect matching index entries
+            if self.index[i][0] != kmer:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
